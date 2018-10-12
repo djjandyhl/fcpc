dfde32a60782a8fa066e49ca4e11a8afbc35cf3a
stefan <djjandyhl@gmail.com>
2018-10-12 14:42:51 +0800

    按首字母排序

--- config/kfs.php
+++ config/kfs.php
@@ -6,15 +6,36 @@
  * Time: 21:57
  */
 return [
-    'data'=>[
+    'data' => [
         [
             'padding' => '.05vh 2.8125vw',
             'fontSize' => '4vw',
             'data' => [
-                ['name' => '万科', 'logFileName' => 'wanke.png'],
-                ['name' => '恒大', 'logFileName' => 'hengda.png'],
+                ['name' => '奥元', 'logFileName' => 'aoyuan.png'],
+                ['name' => '邦泰', 'logFileName' => 'bangtai.png'],
                 ['name' => '保利', 'logFileName' => 'baoli.png'],
+                ['name' => '复地', 'logFileName' => 'fudi.png'],
+
+            ]
+        ],
+        [
+            'padding' => '.05vh 2.8125vw',
+            'fontSize' => '4vw',
+            'data' => [
+                ['name' => '合能', 'logFileName' => 'heneng.png'],
+                ['name' => '恒大', 'logFileName' => 'hengda.png'],
                 ['name' => '华润', 'logFileName' => 'huarun.png'],
+                ['name' => '华宇', 'logFileName' => 'huayu.png'],
+            ]
+        ],
+        [
+            'padding' => '.05vh 2.8125vw',
+            'fontSize' => '4vw',
+            'data' => [
+                ['name' => '金科', 'logFileName' => 'jinke.png'],
+                ['name' => '蓝光', 'logFileName' => 'languang.png'],
+                ['name' => '蓝润', 'logFileName' => 'lanrun.png'],
+                ['name' => '龙光', 'logFileName' => 'longguang.png'],
             ]
         ],
         [
@@ -22,39 +43,9 @@ return [
             'fontSize' => '4vw',
             'data' => [
                 ['name' => '龙湖', 'logFileName' => 'longhu.png'],
+                ['name' => '绿城', 'logFileName' => 'lvcheng.png'],
                 ['name' => '绿地', 'logFileName' => 'lvdi.png'],
                 ['name' => '融创', 'logFileName' => 'rongchuang.png'],
-                ['name' => '中海', 'logFileName' => 'zhonghai.png'],
-            ]
-        ],
-        [
-            'padding' => '.05vh 2.8125vw',
-            'fontSize' => '4vw',
-            'data' => [
-                ['name' => '正荣', 'logFileName' => 'zhengrong.png'],
-                ['name' => '金科', 'logFileName' => 'jinke.png'],
-                ['name' => '合能', 'logFileName' => 'heneng.png'],
-                ['name' => '旭辉', 'logFileName' => 'xuhui.png'],
-            ]
-        ],
-        [
-            'padding' => '.05vh 2.8125vw',
-            'fontSize' => '4vw',
-            'data' => [
-                ['name' => '首创', 'logFileName' => 'shouchuang.png'],
-                ['name' => '华宇', 'logFileName' => 'huayu.png'],
-                ['name' => '蓝光', 'logFileName' => 'languang.png'],
-                ['name' => '复地', 'logFileName' => 'fudi.png'],
-            ]
-        ],
-        [
-            'padding' => '.05vh 2.8125vw',
-            'fontSize' => '4vw',
-            'data' => [
-                ['name' => '蓝润', 'logFileName' => 'lanrun.png'],
-                ['name' => '世茂', 'logFileName' => 'shimao.png'],
-                ['name' => '龙光', 'logFileName' => 'longguang.png'],
-                ['name' => '邦泰', 'logFileName' => 'bangtai.png'],
             ]
         ],
         [
@@ -62,9 +53,21 @@ return [
             'fontSize' => '4vw',
             'data' => [
                 ['name' => '融信', 'logFileName' => 'rongxin.png'],
+                ['name' => '世茂', 'logFileName' => 'shimao.png'],
+                ['name' => '首创', 'logFileName' => 'shouchuang.png'],
+                ['name' => '万科', 'logFileName' => 'wanke.png'],
+            ]
+        ],
+        [
+            'padding' => '.05vh 2.8125vw',
+            'fontSize' => '4vw',
+            'data' => [
+                ['name' => '旭辉', 'logFileName' => 'xuhui.png'],
+                ['name' => '正荣', 'logFileName' => 'zhengrong.png'],
                 ['name' => '置信', 'logFileName' => 'zhixin.png'],
-                ['name' => '奥元', 'logFileName' => 'aoyuan.png'],
-                ['name' => '绿城', 'logFileName' => 'lvcheng.png'],
+                ['name' => '中海', 'logFileName' => 'zhonghai.png'],
+
+
             ]
         ],
         [
@@ -72,9 +75,9 @@ return [
             'fontSize' => '3.5vw',
             'data' => [
                 ['name' => '碧桂园', 'logFileName' => 'biguiyuan.png'],
+                ['name' => '葛洲坝', 'logFileName' => 'gezhouba.png'],
                 ['name' => '新希望', 'logFileName' => 'xinxiwang.png'],
                 ['name' => '雅居乐', 'logFileName' => 'yajule.png'],
-                ['name' => '葛洲坝', 'logFileName' => 'gezhouba.png'],
             ]
         ],
         [
@@ -82,16 +85,16 @@ return [
             'fontSize' => '3.5vw',
             'data' => [
                 ['name' => '中铁建', 'logFileName' => 'zhongtiejian.png'],
-                ['name' => '新城控股', 'logFileName' => 'xinchengkg.png'],
-                ['name' => '中南置地', 'logFileName' => 'zhongnanzd.png'],
+                ['name' => '北大资源', 'logFileName' => 'beidazy.png'],
+                ['name' => '成都宜华', 'logFileName' => 'chengduyh.png'],
             ]
         ],
         [
             'padding' => '.05vh 1vw',
             'fontSize' => '3.5vw',
             'data' => [
-                ['name' => '北大资源', 'logFileName' => 'beidazy.png'],
-                ['name' => '成都宜华', 'logFileName' => 'chengduyh.png'],
+                ['name' => '新城控股', 'logFileName' => 'xinchengkg.png'],
+                ['name' => '中南置地', 'logFileName' => 'zhongnanzd.png'],
             ]
         ]
     ]
